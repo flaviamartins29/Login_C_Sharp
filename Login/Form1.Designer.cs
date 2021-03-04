bebd2dea8eed5dc3aa69ea1bbd018d2f23d5d632
Flavia Martins <flavia.martins1991@hotmail.com>
2021-03-04 09:18:30 -0300

    Scren with Login make in Windows form C#

---
+++ Login/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace Login
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inicio = new System.Windows.Forms.Label();
+            this.nome = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textUsuario = new System.Windows.Forms.TextBox();
+            this.textSenha = new System.Windows.Forms.TextBox();
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inicio
+            // 
+            this.inicio.AutoSize = true;
+            this.inicio.Font = new System.Drawing.Font("Arial Black", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.inicio.Location = new System.Drawing.Point(74, 9);
+            this.inicio.Name = "inicio";
+            this.inicio.Size = new System.Drawing.Size(398, 51);
+            this.inicio.TabIndex = 0;
+            this.inicio.Text = "Acesso ao Sistema";
+            // 
+            // nome
+            // 
+            this.nome.AutoSize = true;
+            this.nome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.nome.Location = new System.Drawing.Point(50, 115);
+            this.nome.Name = "nome";
+            this.nome.Size = new System.Drawing.Size(103, 29);
+            this.nome.TabIndex = 1;
+            this.nome.Text = "Usuário";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(50, 205);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Senha";
+            // 
+            // textUsuario
+            // 
+            this.textUsuario.Location = new System.Drawing.Point(170, 115);
+            this.textUsuario.Name = "textUsuario";
+            this.textUsuario.Size = new System.Drawing.Size(264, 31);
+            this.textUsuario.TabIndex = 3;
+            // 
+            // textSenha
+            // 
+            this.textSenha.Location = new System.Drawing.Point(170, 205);
+            this.textSenha.Name = "textSenha";
+            this.textSenha.PasswordChar = '*';
+            this.textSenha.Size = new System.Drawing.Size(264, 31);
+            this.textSenha.TabIndex = 4;
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.Location = new System.Drawing.Point(193, 273);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(193, 57);
+            this.btnConfirmar.TabIndex = 5;
+            this.btnConfirmar.Text = "Confirma";
+            this.btnConfirmar.UseVisualStyleBackColor = true;
+            this.btnConfirmar.Click += new System.EventHandler(this.btnConfirmar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 450);
+            this.Controls.Add(this.btnConfirmar);
+            this.Controls.Add(this.textSenha);
+            this.Controls.Add(this.textUsuario);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nome);
+            this.Controls.Add(this.inicio);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label inicio;
+        private System.Windows.Forms.Label nome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textUsuario;
+        private System.Windows.Forms.TextBox textSenha;
+        private System.Windows.Forms.Button btnConfirmar;
+    }
+}
+
---
+++ Login/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConfirmar_Click(object sender, EventArgs e)
+        {
+            Form2 novo = new Form2();
+
+            string usuario, senha;
+            usuario = textUsuario.Text;
+            senha = textSenha.Text;
+            
+            if (usuario == "Admin" && senha == "Admin")
+            {
+                novo.Show();
+                this.Visible = false;
+            }
+            else
+            {
+                MessageBox.Show("Senha e ou usuario incorretos", "Menssagem", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
